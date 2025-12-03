63918d76152aa041458ea81e788dfadba351f234
Gustavo Andrade <gugufonceca.2004@gmail.com>
2025-12-03 17:26:39 -0300

    03/12

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,4 @@
-// ===============================
 // CARROSSEL
-// ===============================
-
 const track = document.querySelector(".carousel-track");
 const cards = document.querySelectorAll(".project-card");
 const prevBtn = document.querySelector(".prev");
@@ -10,34 +7,29 @@ const nextBtn = document.querySelector(".next");
 let index = 0;
 const total = cards.length;
 
-// Move o carrossel
 function updateCarousel() {
   track.style.transform = `translateX(-${index * 100}%)`;
 }
 
-// Botão próximo
 nextBtn.addEventListener("click", () => {
   index++;
-  if (index >= total) index = 0; // volta ao primeiro
+  if (index >= total) index = 0;
   updateCarousel();
 });
 
-// Botão anterior
 prevBtn.addEventListener("click", () => {
   index--;
-  if (index < 0) index = total - 1; // vai para o último
+  if (index < 0) index = total - 1;
   updateCarousel();
 });
 
 
-// ===============================
 // ZOOM AO CLICAR NA IMAGEM
-// ===============================
 const overlay = document.getElementById("img-overlay");
 const overlayImg = document.getElementById("overlay-img");
 const thumbs = document.querySelectorAll(".project-thumb img");
 
-// Abre o zoom
+
 thumbs.forEach(img => {
   img.addEventListener("click", () => {
     overlay.style.display = "flex";
@@ -45,20 +37,17 @@ thumbs.forEach(img => {
   });
 });
 
-// Fecha ao clicar no fundo
 overlay.addEventListener("click", () => {
   overlay.style.display = "none";
 });
 
 
-// ===============================
 // EFEITO DIGITAR H2 DO INÍCIO
-// ===============================
 
-const text = "Desenvolvedor Back-End Júnior"; // frase que será digitada
-const speed = 150;        // velocidade digitando (ms)
-const eraseSpeed = 90;   // velocidade apagando (ms)
-const pause = 4000;      // tempo com a frase completa (ms)
+const text = "Desenvolvedor Back-End Júnior";
+const speed = 150;
+const eraseSpeed = 90;
+const pause = 4000;
 
 let i = 0;
 let typing = true;
@@ -67,7 +56,6 @@ function typeEffect() {
   const h2 = document.getElementById("type-text");
 
   if (typing) {
-    // escrever
     h2.textContent = text.substring(0, i);
     i++;
 
@@ -75,11 +63,10 @@ function typeEffect() {
       setTimeout(typeEffect, speed);
     } else {
       typing = false;
-      setTimeout(typeEffect, pause); // pausa com frase completa
+      setTimeout(typeEffect, pause);
     }
 
   } else {
-    // apagar
     h2.textContent = text.substring(0, i);
     i--;
 
@@ -87,7 +74,7 @@ function typeEffect() {
       setTimeout(typeEffect, eraseSpeed);
     } else {
       typing = true;
-      setTimeout(typeEffect, 700); // pausa antes de digitar novamente
+      setTimeout(typeEffect, 700);
     }
   }
 }
